91ea97a8b995860903155e9faec44dda304f93b3
Valentine Radchuk <zaknafeyn@gmail.com>
2015-05-28 13:00:34 +0300

    revert install folder

--- phpbb3/config.php
+++ phpbb3/config.php
@@ -2,15 +2,15 @@
 // phpBB 3.0.x auto-generated configuration file
 // Do not change anything in this file!
 $dbms = 'mysql';
-$dbhost = 'localhost';
+$dbhost = 'dns-hosting.cloudapp.net';
 $dbport = '3306';
-$dbname = 'phpbb3_ultimate';
-$dbuser = 'phpbb3_ultimate';
-$dbpasswd = 'phpbb3_ultimate';
+$dbname = 'phpbb3';
+$dbuser = 'ultimate';
+$dbpasswd = 'ultimate';
 $table_prefix = 'phpbb3_';
 $acm_type = 'file';
 $load_extensions = '';
 
-@define('PHPBB_INSTALLED', true);
+@define('PHPBB_INSTALLED', false);
 // @define('DEBUG', true);
 // @define('DEBUG_EXTRA', true);
