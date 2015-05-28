e54abf6d99da53341cbb11365aa6aaa01e9c2a08
Valentine Radchuk <zaknafeyn@gmail.com>
2015-05-28 14:00:35 +0300

    Remove cached files from sources. Add config on working copy of forum

--- Ultimate site/forum/config.php
+++ Ultimate site/forum/config.php
@@ -1,49 +1,16 @@
 <?php
 // phpBB 3.0.x auto-generated configuration file
 // Do not change anything in this file!
-
 $dbms = 'mysqli';
-/*
-$dbhost = 'ultimate.com.ua';//'localhost';
-$dbport = '3306';
-$dbname = 'ultimate_forum';//'new_forum';
-$dbuser = 'ultimate_forum';//'root';
-$dbpasswd = 'ultimateforum2010';// 'root';
-$table_prefix = 'phpbb_';
-  */
-$dbhost = 'localhost';
-$dbport = '3306';
-$dbname = 'new_forum';
-$dbuser = 'root';
-$dbpasswd = 'root';
-$table_prefix = 'phpbb_';
-
-
+$dbhost = 'dns-hosting.cloudapp.net';
+$dbport = '';
+$dbname = 'phpbb3';
+$dbuser = 'ultimate';
+$dbpasswd = 'ultimate';
+$table_prefix = 'phpbb3_';
 $acm_type = 'file';
 $load_extensions = '';
 
 @define('PHPBB_INSTALLED', true);
 // @define('DEBUG', true);
 // @define('DEBUG_EXTRA', true);
-
-
-
-/*
-
-// phpBB 3.0.x auto-generated configuration file
-// Do not change anything in this file!
-$dbms = 'mysqli';
-$dbhost = 'localhost';
-$dbport = '3306';
-$dbname = 'ultimate_forum';//'new_forum';
-$dbuser = 'ultimate_forum';//'root';
-$dbpasswd = 'ultimateforum2010';// 'root';
-$table_prefix = 'phpbb_';
-$acm_type = 'file';
-$load_extensions = '';
-
-@define('PHPBB_INSTALLED', true);
-// @define('DEBUG', true);
-// @define('DEBUG_EXTRA', true);
-*/
-?>
