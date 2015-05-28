b9c00771676e746abb2ef34b0bc3c81ab487356e
Valentine Radchuk <zaknafeyn@gmail.com>
2015-05-28 13:38:21 +0300

    update config.php

--- phpbb3/config.php
+++ phpbb3/config.php
@@ -1,9 +1,9 @@
 <?php
 // phpBB 3.0.x auto-generated configuration file
 // Do not change anything in this file!
-$dbms = 'mysql';
+$dbms = 'mysqli';
 $dbhost = 'dns-hosting.cloudapp.net';
-$dbport = '3306';
+$dbport = '';
 $dbname = 'phpbb3';
 $dbuser = 'ultimate';
 $dbpasswd = 'ultimate';
@@ -11,6 +11,6 @@ $table_prefix = 'phpbb3_';
 $acm_type = 'file';
 $load_extensions = '';
 
-@define('PHPBB_INSTALLED', false);
+@define('PHPBB_INSTALLED', true);
 // @define('DEBUG', true);
 // @define('DEBUG_EXTRA', true);
